707790c1ff2d198b4dc6639b79c2734009d8645a
Aniket Magadum <aniket.magadum@sts.in>
2019-10-17 14:31:30 +0530

    Add GoodMorning function

---
+++ src/HelloWorld/SayGoodMorning.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HelloWorld;
+
+class SayGoodMorning
+{
+    public static function world()
+    {
+        return 'Good morning, Composer!';
+    }
+}
--- tests/test.php
+++ tests/test.php
@@ -2,5 +2,8 @@
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 use HelloWorld\SayHello;
 use HelloWorld\SayBye;
+use HelloWorld\SayGoodMorning;
+
 echo SayHello::world();
 echo SayBye::world();
+echo SayGoodMorning::world();
